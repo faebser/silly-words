e98b5148d5bf4fdcefd35787ea1734b33e64b929
faebser <fabian@esf-frei.ch>
2018-10-15 21:57:31 +0200

    added button

--- buttons.py
+++ buttons.py
@@ -3,21 +3,31 @@ import RPi.GPIO as GPIO
 import time
 
 BUTTON_A = 26
+BUTTON_B = 19
 
-time.sleep(10)
 
 from pyautogui import press
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.add_event_detect(BUTTON_A, GPIO.RISING, bouncetime=200)
+GPIO.add_event_detect(BUTTON_A, GPIO.RISING, bouncetime=200)
+
 
 def callback(pin):
 	print("pressed pin {}".format(pin))
-	press('N')
+	if pin == BUTTON_A:
+		press('N')
+		return
+	if pin == BUTTON_B:
+		press('L')
+		return
+
 
 GPIO.add_event_callback(BUTTON_A, callback)
+GPIO.add_event_callback(BUTTON_B, callback)
 
 
 while True:
